5c0be7cd313b713ff8263a85c98e6a55c51e8fc3
Gustavo Fantato <gustavo.fernandes@monks.com>
2025-12-08 13:18:34 -0300

    feat: Adicionado função Soma

--- fanta/main.go
+++ fanta/main.go
@@ -6,4 +6,17 @@ func main(){
 
 	fmt.Println("Olá, equipe! Eu sou o Fanta, será um prazer aprender e trabalhar com vocês! :)");
 
+	var n1 int;
+	var n2 int;
+
+	fmt.Println("Insira os valores de n1 e n2");
+	fmt.Scan(&n1, &n2);
+	fmt.Println("A soma é:", soma(n1, n2));
+
+
+}
+
+
+func soma(n1 int , n2 int) int{
+	return n1 + n2;
 }
