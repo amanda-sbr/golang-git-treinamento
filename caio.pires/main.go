3d658062892d02f4076968a55b30f29750037dd1
caiopires-monks <caio.pires@monks.com>
2025-12-08 13:27:23 -0300

    feat: Adiciona impressão de slice

--- caio.pires/main.go
+++ caio.pires/main.go
@@ -6,6 +6,12 @@ func main() {
 	fmt.Printf("Opa, meu nome é Caio e eu sou estagiário da Monks!\n\n")
 
 	fmt.Printf("Resultado da soma: %v\n\n", soma(5, 5))
+
+	frutas := []string{"Maçã", "Uva", "Banana"}
+
+	for index, fruta := range frutas {
+		fmt.Printf("Fruta n° %v: %s\n", index, fruta)
+	}
 }
 
 func soma(a int, b int) int {
