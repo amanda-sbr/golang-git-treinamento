7888f1ada3bd34222c941665752e00c30d3e400c
Luiz Felipe (Poru) <luiz.costa@monks.com>
2025-12-08 11:54:57 -0300

    feat: Add age map (tbh, I forgot everyone's age, so they're randos 😅)

--- lfelipediniz-monks/main.go
+++ lfelipediniz-monks/main.go
@@ -21,6 +21,27 @@ func main() {
 		fmt.Println()
 	}
 
+	fmt.Println("\n")
+
+	// map onde a chave eh uma string (nome) e o valor eh um int (idade) 
+	// do pessoal que estava na call
+	idades := map[string]int{
+		"Fanta": 20,
+		"Poru": 21,
+		"Ana": 22,
+		"Brennda": 23,
+		"Farinha": 24,
+		"Duda": 25,
+		"Isarao": 26,
+		"Lisso": 27,
+		"Le": 28,
+		"Madu": 29,
+	}
+
+	for nome, idade := range idades {
+		fmt.Println(nome, idade)
+	}
+
 }
 
 func soma(a int, b int) int {
