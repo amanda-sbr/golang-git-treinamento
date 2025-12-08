0b414c44a82cf04f216659301c4f81bd75f9986c
alexandre <alexandre.junior@monks.com>
2025-12-08 15:37:23 -0300

    feat: Adiciona lista de frutas

--- le/main.go
+++ le/main.go
@@ -3,8 +3,17 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("Olá, equipe! Meu nome é Alexandre, ou Lê, e estou pronto para começar os trabalhos!")
+
 	fmt.Println("A soma é:", soma(3, 5))
+
+	fmt.Println()
+	frutas := []string{"Morango", "Abacate", "Laranja"}
+	for _, fruta := range frutas {
+		fmt.Println(fruta)
+	}
+
 }
 
 func soma(a int, b int) int {
