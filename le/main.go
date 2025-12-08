cdcddbbca27581f48bd612dc73322efb1bc15979
alexandre <alexandre.junior@monks.com>
2025-12-08 15:25:51 -0300

    feat: Adiciona função de soma

--- le/main.go
+++ le/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	//Mensagem à equipe
 	fmt.Println("Olá, equipe! Meu nome é Alexandre, ou Lê, e estou pronto para começar os trabalhos!")
+	fmt.Println("A soma é:", soma(3, 5))
+}
+
+func soma(a int, b int) int {
+	return a + b
 }
