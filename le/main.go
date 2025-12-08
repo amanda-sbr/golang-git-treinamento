50ca63023608b86201b7df6da429ef3f413ba909
alexandre <alexandre.junior@monks.com>
2025-12-08 16:02:19 -0300

    feat: Adiciona comentarios

--- le/main.go
+++ le/main.go
@@ -6,23 +6,23 @@ import (
 )
 
 func main() {
-
+	// Início
 	fmt.Println("Olá, equipe! Meu nome é Alexandre, ou Lê, e estou pronto para começar os trabalhos!")
-
+	// Desafio 1
 	fmt.Println("A soma é:", soma(3, 5))
-
+	// Desafio 2
 	fmt.Println()
 	frutas := []string{"Morango", "Abacate", "Laranja"}
 	for _, fruta := range frutas {
 		fmt.Println(fruta)
 	}
-
+	// Desafio 3
 	fmt.Println()
 	for i := 1; i < 6; i++ {
 		linha := strings.Repeat("*", i)
 		fmt.Println(linha)
 	}
-
+	// Desafio bônus
 	fmt.Println()
 	colegas := map[string]int{
 		"lê":      27,
@@ -38,6 +38,7 @@ func main() {
 
 }
 
+// função soma
 func soma(a int, b int) int {
 	return a + b
 }
