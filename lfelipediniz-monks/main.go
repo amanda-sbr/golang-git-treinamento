3999c636281f6cd5bbe65a9690b9ec39eee7949c
Luiz Felipe (Poru) <luiz.costa@monks.com>
2025-12-08 11:26:16 -0300

    feat: add basic sum function

--- lfelipediniz-monks/main.go
+++ lfelipediniz-monks/main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Olá, mundo! Meu nome é Poru e estou pronto para começar!")
+	fmt.Println(soma(1, 2))
 }
+
+func soma(a int, b int) int {
+	return a + b
+}
+
+
+
+
+
+
