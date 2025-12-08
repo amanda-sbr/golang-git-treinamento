f2496e2dd70af7aa8e15da61816346c37e4bee78
caiopires-monks <caio.pires@monks.com>
2025-12-08 13:57:45 -0300

    feat: Adiciona desafio bônus (map de idades)

--- caio.pires/go.mod
+++
@@ -1,3 +0,0 @@
-module treino
-
-go 1.25.5
--- caio.pires/main.go
+++ caio.pires/main.go
@@ -6,19 +6,35 @@ import (
 )
 
 func main() {
+	//Inicio
 	fmt.Printf("Opa, meu nome é Caio e eu sou estagiário da Monks!\n\n")
 
+	//Desafio 1
 	fmt.Printf("Resultado da soma: %v\n\n", soma(5, 5))
 
+	//Desafio 2
 	frutas := []string{"Maçã", "Uva", "Banana"}
 
 	for index, fruta := range frutas {
 		fmt.Printf("Fruta n° %v: %s\n", index, fruta)
 	}
 
+	//Desafio 3
 	fmt.Println()
 	for i := 1; i <= 5; i++ {
 		fmt.Printf("%s\n", strings.Repeat("*", i))
+	}
+
+	//Bônus (não sei se as idades estão certas)
+	fmt.Println()
+	idadesColegas := map[string]int{
+		"Poru":    21,
+		"Fanta":   18,
+		"Farinha": 24,
+		"Lê":      30,
+	}
+	for nome, idade := range idadesColegas {
+		fmt.Printf("Nome:%s, Idade: %v\n", nome, idade)
 	}
 }
 
