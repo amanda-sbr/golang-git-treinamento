4759fdb43ba9c7fb040d9742c481585a87bc9819
Luiz Felipe (Poru) <luiz.costa@monks.com>
2025-12-08 11:28:31 -0300

    feat: add slice iteration

--- lfelipediniz-monks/main.go
+++ lfelipediniz-monks/main.go
@@ -5,6 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Olá, mundo! Meu nome é Poru e estou pronto para começar!")
 	fmt.Println(soma(1, 2))
+
+	// slice de strings com 3 frutas
+	frutas := []string{"maçã", "banana", "laranja"}
+
+	for i, fruta := range frutas {
+		fmt.Println(i, fruta)
+	}
 }
 
 func soma(a int, b int) int {
